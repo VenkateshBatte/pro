1bce43ba19807962f9d851b548d7cba059796bc2
venkatesh <battevenketesh@hmail.com>
2023-06-01 15:26:53 +0530

    second commit

--- pro1.c
+++ pro1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(){
-	printf("i am venkatesh");
+	printf("i am venkatesh \n ");
+	printf("this is changed by aswin \n ");
 	return 0;
 }
--- pro2.c
+++ pro2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(){
-	printf("i am aswin");
+	printf("i am aswin \n ");
+	printf("this is changed by venkey \n ");
 	return 0;
 }
